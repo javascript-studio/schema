f43b00cdf9d13e0398ca2ecdfca7800d2f6f89cb
Maximilian Antoni <mail@maxantoni.de>
2021-05-22 13:14:02 +0200

    Extract schema to own file

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { SCHEMA_VALIDATION } = require('./lib/constants');
+const { opt } = require('./lib/opt');
+const { one } = require('./lib/one');
+const { object } = require('./lib/object');
+const { array } = require('./lib/array');
+const { map } = require('./lib/map');
+const { schema } = require('./lib/schema');
+
+schema.spec = schema;
+
+schema.opt = opt;
+
+schema.one = one;
+
+schema.object = object;
+
+schema.array = array;
+
+schema.map = map;
+
+schema.SCHEMA_VALIDATION = SCHEMA_VALIDATION;
+
+module.exports = schema;
