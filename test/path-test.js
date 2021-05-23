34f8f247115962f5c0ea5d32d11396fa71e90fe8
Maximilian Antoni <mail@maxantoni.de>
2021-05-23 16:29:13 +0200

    Rename path helper to objectPath

--- test/path-test.js
+++ test/path-test.js
@@ -2,17 +2,17 @@
 'use strict';
 
 const { assert } = require('@sinonjs/referee-sinon');
-const { path, arrayPath } = require('../lib/path');
+const { objectPath, arrayPath } = require('../lib/path');
 
-describe('path', () => {
+describe('objectPath', () => {
   it('returns key if parent is null', () => {
-    const p = path(null, 'key');
+    const p = objectPath(null, 'key');
 
     assert.equals(p, 'key');
   });
 
   it('returns parent and key', () => {
-    const p = path('parent', 'key');
+    const p = objectPath('parent', 'key');
 
     assert.equals(p, 'parent.key');
   });
