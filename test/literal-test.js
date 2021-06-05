3fa0875b6bcd2d710d60eab6ad815a1998a672a2
Maximilian Antoni <mail@maxantoni.de>
2021-06-05 11:12:21 +0200

    Remove obsolete mocha env comments

--- test/literal-test.js
+++ test/literal-test.js
@@ -1,4 +1,3 @@
-/*eslint-env mocha*/
 'use strict';
 
 const { assert, refute } = require('@sinonjs/referee-sinon');
--- test/spec-function-test.js
+++ test/spec-function-test.js
@@ -1,4 +1,3 @@
-/*eslint-env mocha*/
 'use strict';
 
 const { assert, refute, sinon } = require('@sinonjs/referee-sinon');
