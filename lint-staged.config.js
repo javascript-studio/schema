6e17968a91dafdc06b991dd10bd351229d60b220
Maximilian Antoni <mail@maxantoni.de>
2021-06-05 11:26:00 +0200

    Add Studio Related Tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const withRelatedTests = require('@studio/related-tests');
+
 module.exports = {
-  '*.js': 'eslint --fix',
+  '*.js': ['eslint --fix', withRelatedTests('mocha')],
   '*.{js,json,md,yml}': 'prettier --write'
 };
