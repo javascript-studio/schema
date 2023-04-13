3df05eaa3d7bc1ebb23685838a405977c1b61a35
Maximilian Antoni <mail@maxantoni.de>
2023-04-13 17:36:08 +0200

    Make API strict and add types

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { assert, refute, sinon } = require('@sinonjs/referee-sinon');
-const { schema } = require('./index');
+const { schema, object, boolean } = require('./index');
 
 describe('schema', () => {
   afterEach(() => {
@@ -9,7 +9,7 @@ describe('schema', () => {
   });
 
   it('returns a function with read, write and verify', () => {
-    const validate = schema({});
+    const validate = schema(object({}));
 
     assert.isFunction(validate);
     assert.isFunction(validate.read);
@@ -17,52 +17,70 @@ describe('schema', () => {
     assert.isFunction(validate.verify);
   });
 
-  it('exposes spec type Object', () => {
-    const spec = { test: schema.number };
+  it('exposes type Object for object validator', () => {
+    const test = object({ test: schema.number });
 
-    const validate = schema(spec);
+    const validate = schema(test);
 
     assert.same(validate.type, 'Object');
     assert.equals(validate.properties, { test: schema.number });
+    // @ts-expect-error
+    assert.isUndefined(validate.items);
   });
 
-  it('exposes spec type Array', () => {
-    const spec = [schema.number];
+  it('exposes type Array', () => {
+    const test = schema.array(schema.number);
 
-    const validate = schema(spec);
+    const validate = schema(test);
 
     assert.same(validate.type, 'Array');
     assert.same(validate.items, schema.number);
+    // @ts-expect-error
+    assert.isUndefined(validate.properties);
   });
 
-  it('exposes spec type number', () => {
+  it('exposes type number', () => {
     const validate = schema(schema.number);
 
     assert.same(validate.type, 'number');
+    // @ts-expect-error
+    assert.isUndefined(validate.properties);
+    // @ts-expect-error
+    assert.isUndefined(validate.items);
   });
 
-  it('exposes spec type string', () => {
+  it('exposes type string', () => {
     const validate = schema(schema.string);
 
     assert.same(validate.type, 'string');
   });
 
   it('does not invoke the given validator property accessors', () => {
-    const object = schema.object({});
-    const read = sinon.replaceGetter(object.verify, 'read', sinon.fake());
-    const write = sinon.replaceGetter(object.verify, 'write', sinon.fake());
+    const obj = schema.object({});
+    const read = sinon.replaceGetter(obj['verify'], 'read', sinon.fake());
+    const write = sinon.replaceGetter(obj['verify'], 'write', sinon.fake());
 
-    schema(object);
+    schema(obj);
 
     refute.called(read);
     refute.called(write);
   });
 
+  it('returns the validated object', () => {
+    const validate = schema(object({ test: boolean }));
+
+    const result = validate({ test: true });
+
+    assert.isObject(result);
+    assert.isTrue(result.test); // shouldn't file type check
+  });
+
   it('fails with default error code', () => {
-    const validate = schema({});
+    const validate = schema(object({}));
 
     assert.exception(
       () => {
+        // @ts-expect-error
         validate(null);
       },
       { code: 'E_SCHEMA' }
@@ -70,10 +88,11 @@ describe('schema', () => {
   });
 
   it('fails with error code from schema option', () => {
-    const validate = schema({}, { error_code: 'INVALID' });
+    const validate = schema(object({}), { error_code: 'INVALID' });
 
     assert.exception(
       () => {
+        // @ts-expect-error
         validate(null);
       },
       { code: 'INVALID' }
@@ -81,17 +100,21 @@ describe('schema', () => {
   });
 
   it('fails in reader with error code from schema option', () => {
-    const validate = schema({}, { error_code: 'INVALID' });
+    const validate = schema(object({ test: boolean }), {
+      error_code: 'INVALID'
+    });
 
     assert.exception(
       () => {
+        // @ts-expect-error
         validate.read(null);
       },
       { code: 'INVALID' }
     );
-    const valid = validate.read({});
+    const valid = validate.read({ test: true });
     assert.exception(
       () => {
+        // @ts-expect-error
         return valid.invalid;
       },
       { code: 'INVALID' }
@@ -99,10 +122,11 @@ describe('schema', () => {
   });
 
   it('fails in writer with error code from schema option', () => {
-    const validate = schema({}, { error_code: 'INVALID' });
+    const validate = schema(object({}), { error_code: 'INVALID' });
 
     assert.exception(
       () => {
+        // @ts-expect-error
         validate.write(null);
       },
       { code: 'INVALID' }
